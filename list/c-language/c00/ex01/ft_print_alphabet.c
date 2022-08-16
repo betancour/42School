9f466a02ba220dd9ccc1668b376e54093a11cb66
Yitzhak B. Solorzano <betancour@gmail.com>
2022-08-15 22:15:08 -0300

    first commit

---
+++ list/c-language/c00/ex01/ft_print_alphabet.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_print_alphabet(void)
+{
+	char index;
+
+	index = 'a';
+
+	while (index <= 'z')
+	{
+		ft_putchar(index);
+		index++;
+	}
+}
