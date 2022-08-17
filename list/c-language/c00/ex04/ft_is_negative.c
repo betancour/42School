c81f87b2c0d194cf82b758ede478f10f9911f047
Yitzhak B. Solorzano <betancour@gmail.com>
2022-08-16 23:22:37 -0300

    ex 04 done

--- list/c-language/c00/ex04/ft_is_negative.c
+++ list/c-language/c00/ex04/ft_is_negative.c
@@ -1,13 +1,18 @@
 #include <unistd.h>
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
 void ft_is_negative(int n)
 {
-	int index;
-
-	index = 0;
-
-	if{index < 0}
+	if(n < 0)
 	{
-
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar('P');
 	}
 }
