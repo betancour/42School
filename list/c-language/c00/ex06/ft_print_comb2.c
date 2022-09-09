7917549905aa245578de18e4bb6e14fe016e8342
Yitzhak B. Solorzano <betancour@gmail.com>
2022-09-09 01:25:40 -0300

    ex06 completed and ex07

--- list/c-language/c00/ex06/ft_print_comb2.c
+++ list/c-language/c00/ex06/ft_print_comb2.c
@@ -12,15 +12,16 @@ void ft_print_comb2(void)
     i = -1;
     while(++i <= 98)
     {
-        j = 1;
+        j = 0;
         while(++j <= 99)
         {
             ft_putchar((char)(i/10) + '0');
             ft_putchar((char)(i%10) + '0');
+            ft_putchar(' ');
             ft_putchar((char)(j/10) + '0');
             ft_putchar((char)(j%10) + '0');
 
-            if((i != 98))
+            if( (j != 99))
             {
                 ft_putchar(',');
                 ft_putchar(' ');
