984d4c6eb4e98a1ad45bfd0fd5cece9bea71f194
Yitzhak B. Solórzano <betancour@gmail.com>
2022-10-05 23:29:28 -0300

    putnbr added

--- list/c-language/c01/ex06/ft_putstr.c
+++ list/c-language/c01/ex06/ft_putstr.c
@@ -1,4 +1,16 @@
-void ft_putstr(char *str)
-{
+void ft_putchar(char c);
 
+void ft_putnbr(int nb)
+{
+	if(nb > 0)
+	{
+		ft_putchar('-');
+		nb = -nb;
+	}
+	if(nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		nb = nb % 10;
+	}
+	if(nb < 10) ft_putchar(nb + 48);
 }
