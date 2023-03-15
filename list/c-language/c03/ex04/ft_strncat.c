735afedc9b7ba4dba24d450cf56d325b0257684e
Yitzhak B. Solórzano <betancour@gmail.com>
2023-03-15 02:08:59 -0300

    c03 added

---
+++ list/c-language/c03/ex04/ft_strncat.c
@@ -0,0 +1,17 @@
+char *ft_strncat(char *dest, char *src, unsigned int nb)
+{
+    unsigned int dest_len;
+    unsigned int i;
+
+    dest_len = 0;
+    while (dest[dest_len] != '\0')
+        dest_len++;
+    i = 0;
+    while (i < nb && src[i] != '\0')
+    {
+        dest[dest_len + i] = src[i];
+        i++;
+    }
+    dest[dest_len + i] = '\0';
+    return (dest);
+}
---
+++ list/c-language/c03/ex06/ft_strlcat.c
@@ -0,0 +1,18 @@
+int ft_strlen(char *str);
+
+unsigned int     ft_strlcat(char *dest, char *src, unsigned int size)
+{
+    unsigned int    dest_len = ft_strlen(dest);
+    unsigned int    src_len = ft_strlen(src);
+    unsigned int    i;
+
+    if (size <= dest_len)
+        return src_len + size;
+
+    for (i = 0; i < size - dest_len - 1 && src[i] != '\0'; i++)
+        dest[dest_len + i] = src[i];
+    dest[dest_len + i] = '\0'; // add null byte at end
+
+    return dest_len + src_len;
+}
+
