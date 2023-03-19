c8b20f3d91a6c546a4d5ffd7ff7424643a56953e
Yitzhak B. Solórzano <betancour@gmail.com>
2023-03-19 00:18:29 -0300

    list C06 ready

---
+++ list/c-language/c06/ex03/ft_rev_params.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+
+void     ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void     ft_putstr(char *str)
+{
+    while(*str != '\0')
+    {
+        ft_putchar(*str);
+        str++;
+    }
+}
+
+int     main(int argc, char **argv)
+{
+    while(argc > 1)
+    {
+        ft_putstr(argv[argc -1]);
+        ft_putchar('\n');
+        argc--;
+    }
+    return (0);
+}
