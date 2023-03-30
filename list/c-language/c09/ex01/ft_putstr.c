a56c720f3d1460b6d9cefb09fe2458969e9f8d5c
Yitzhak B. Solórzano <betancour@gmail.com>
2023-03-29 21:45:59 -0300

    corrected

--- list/c-language/c09/ex01/ft_putstr.c
+++ list/c-language/c09/ex01/ft_putstr.c
@@ -1,12 +1,12 @@
-void    ft_putchar(char *c);
+void    ft_putchar(char c);
 
 void    ft_putstr(char *str)
 {
     if(str)
     {
-        while (*str)
+        while (*str != '\0')
         {
-            ft_putchar(str);
+            ft_putchar(*str);
             str++;
         }        
     }
