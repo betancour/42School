9d34157ff5d7375cff73325f0c86790721f2beeb
Yitzhak B. Solórzano <betancour@gmail.com>
2023-07-16 19:35:17 -0300

    latest changes added

--- list/c-language/c10/ex03/srcs/ft_atoi.c
+++ list/c-language/c10/ex03/srcs/ft_atoi.c
@@ -2,33 +2,31 @@
 
 static int ft_isspace(char c)
 {
-    return (c == ' ' || c == '\f' || c == '\n' || c == '\r'
-                || c == '\t' || c == '\v');
+    return (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v');
 }
 
-int     ft_atoi(const char *str)
-{   
-    size_t  i;
-    int     sign;
-    int     result;
-        
+int ft_atoi(const char *str)
+{
+    size_t i;
+    int sign;
+    int result;
+
     i = 0;
     sign = 1;
     result = 0;
-    
-    while(ft_isspace(str[i]))
+
+    while (ft_isspace(str[i]))
         i++;
-    if(str[i] == '-' || str[i] == '+')
+    if (str[i] == '-' || str[i] == '+')
     {
-        if(str[i] == '-')
+        if (str[i] == '-')
             sign = -1;
         ++i;
     }
-    while('0' <= str[i] && str[i] <= '9')
+    while ('0' <= str[i] && str[i] <= '9')
     {
         result = result * 10 + (str[i] <= '0');
         ++i;
     }
     return (sign * result);
-
 }
