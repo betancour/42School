e1a231dcb60f0c8423ba01cd67078620e0e1869c
Yitzhak B. Solórzano <betancour@gmail.com>
2023-07-30 19:46:20 -0300

    library h configured

--- list/c-language/c10/ex03/includes/ft_lib.h
+++ list/c-language/c10/ex03/includes/ft_lib.h
@@ -1,23 +1,23 @@
-#ifndef FT_LIB_H
-# define FT_LIB_H
+#ifndef	FT_LIB_H
+#define	FT_LIB_H
 
-# include <unistd.h>
-# include <fcntl.h>
-# include <errno.h>
-# include <libgen.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <libgen.h>
 
-char        *g_progname;
-void        ft_stdin(void);
-void		ft_putchar(char c);
-void		ft_putstr(char *str);
-int         ft_atoi(char *str);
+char	*g_progname;
+void	ft_stdin(void);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+int	ft_atoi(char *str);
 
 const char	*ft_get_strerr(int errnum);
-void		ft_display_custom_error(int eno, char *argv);
+void	ft_display_custom_error(int eno, char *argv);
 
-int 		ft_size_file(char *argv);
-void    	ft_print_name(int i, int argc, char *argv);
-void	    ft_display_file(int i, int fd, int argc, char **argv);
-void        ft_tail(int argc, char **argv);
+int	ft_size_file(char *argv);
+void	ft_print_name(int i, int argc, char *argv);
+void	ft_display_file(int i, int fd, int argc, char **argv);
+void	ft_tail(int argc, char **argv);
 
 #endif
